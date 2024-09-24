71c1ec5ef42bbc39bdfcf5969e3cafc8901f3f53
akanksha pawar <akankshapawar131202@gmail.com>
2024-09-24 23:04:43 +0530

    second commit

--- src/Page1/Page1.jsx
+++ src/Page1/Page1.jsx
@@ -4,48 +4,69 @@ const Page1 = () => {
   return (
     <div className='mt-2 '>
      <p className='text-xl font-bold pl-10  '> Lek Ladki Yojana Maharashtra:  लेक लाडकी योजनेतून मुलींना मिळणार 1 लाख 1 हजार रुपये, घरी बसून असे करा अर्ज ..... </p>
-    <div className=' justify-center items-center mt-10 '>
-        <img src={photo4} className='h-half w-screen'/>
+    <div className=' justify-center items-center mt-10 sm: pl-60 '>
+        <img src={photo4} className='h-half ss:w-screen'/>
     </div>
     <div className='mt-4 align-baseline'><p>मुलींचे भविष्य उज्ज्वल करण्यासाठी सरकार सर्वतोपरी प्रयत्न करत असून, त्यासाठी नवनवीन योजनाही सुरू केल्या जात आहेत. त्याचप्रमाणे आता महाराष्ट्र सरकारने मुलींसाठी लेच लाडकी योजना सुरू केली असून, त्याद्वारे मुलींच्या जन्मापासून ते १८ वर्षे पूर्ण होईपर्यंत सरकार त्यांना १ लाख १ हजार रुपयांची मदत करणार आहे.</p>
         <p>सरकार मुलींना ही मदत रक्कम हप्त्याच्या स्वरूपात देईल जी थेट मुलींच्या बँक खात्यात हस्तांतरित केली जाईल. महाराष्ट्र सरकारने सुरू केलेल्या लेक लाडकी योजनेंतर्गत राज्यातील मुलींना जन्माच्या वेळी पहिला हप्ता मिळणार आहे. शेवटच्या हप्त्याचे पैसे 18 वर्षे पूर्ण झाल्यानंतर सरकार जारी करेल. लेच लाडकी योजनेचा लाभ घेऊ इच्छिणाऱ्या महाराष्ट्र राज्यातील इच्छुक मुली अर्ज करू शकतात. या पोस्टमध्ये तुम्हाला लेक लाडकी योजनेबद्दल संपूर्ण माहिती मिळेल, त्यामुळे लेखात शेवटपर्यंत रहा.
 
         </p>
         <div className=' justify-center items-center'>
+          <h1 className='text-xl font-bold mt-4'>Lek Ladki Yojana Maharashtra 2023 Key Points
+          </h1>
         <table className='border-1 border-solid border-black w-11/12 ml-4 text-center justify-center'>
   <tr className='border-2 border-solid border-black'>
-    <td className='border-2 border-solid border-black'>Emil</td>
-    <td className='border-2 border-solid border-black'>Tobias</td>
+    <td className='border-2 border-solid border-black'>योजनेचे नाव
+    </td>
+    <td className='border-2 border-solid border-black'>महाराष्ट्र लेक लाडकी योजना
+    </td>
   </tr>
   <tr className='border-2 border-solid border-black'>
-    <td className='border-2 border-solid border-black'>16</td>
-    <td className='border-2 border-solid border-black'>14</td>
+    <td className='border-2 border-solid border-black'>योजनेचा उद्देश
+    </td>
+    <td className='border-2 border-solid border-black'>राज्यात मुलींचा जन्म आणि त्यांच्या शिक्षणाला चालना देण्यासाठी आर्थिक सहाय्य प्रदान करणे.
+    </td>
   </tr>
   <tr className='border-2 border-solid border-black'>
-    <td className='border-2 border-solid border-black'>16</td>
-    <td className='border-2 border-solid border-black'>14</td>
+    <td className='border-2 border-solid border-black'>आर्थिक सहाय्य
+    </td>
+    <td className='border-2 border-solid border-black'>एकूण १,०१,०००/- रुपए
+    </td>
   </tr>
   <tr className='border-2 border-solid border-black'>
-    <td className='border-2 border-solid border-black'>16</td>
-    <td className='border-2 border-solid border-black'>14</td>
+    <td className='border-2 border-solid border-black'>योजनेची सुरुवात
+    </td>
+    <td className='border-2 border-solid border-black'>ऑक्टोबर 2023
+    </td>
   </tr>
   <tr className='border-2 border-solid border-black'>
-    <td className='border-2 border-solid border-black'>16</td>
-    <td className='border-2 border-solid border-black'>14</td>
+    <td className='border-2 border-solid border-black'>योजनेचे क्षेत्र
+    </td>
+    <td className='border-2 border-solid border-black'>राज्य सरकार (महाराष्ट्र)
+    </td>
   </tr>
   <tr className='border-2 border-solid border-black'>
-    <td className='border-2 border-solid border-black'>16</td>
-    <td className='border-2 border-solid border-black'>14</td>
+    <td className='border-2 border-solid border-black'>विभाग / योजना मंत्रालय
+    </td>
+    <td className='border-2 border-solid border-black'>महिला व बालविकास विभाग,महाराष्ट्र
+    </td>
   </tr>
   <tr className='border-2 border-solid border-black'>
-    <td className='border-2 border-solid border-black'>16</td>
-    <td className='border-2 border-solid border-black'>14</td>
+    <td className='border-2 border-solid border-black'>योजनेचे लाभार्थी
+
+    </td>
+    <td className='border-2 border-solid border-black'>राज्य के पीले आणि नारंगी राशन कार्डधारक परिवार की सर्व बालके.
+    </td>
   </tr>
 </table>
-</div>
 
+</div>
+<div className='mt-4'>  <p >जन्मदर वाढवण्यासाठी आणि मुलांच्या शिक्षणाला चालना देण्यासाठी, माझी कन्या भाग्यश्री (साधू अहरत) नवाहन योजना 1 ऑगस्ट 2017 च्या शासन आदेशानुसार लागू करण्यात आली होती. या योजनेला मिळालेला अपुरा प्रतिसाद पाहता, ती सुरू ठेवणे सरकारच्या निर्णयावर अवलंबून आहे. मुलींच्या सक्षमीकरणासाठी एक नवीन उपक्रम सुरू करणे. 2023-24 या आर्थिक वर्षाच्या त्यांच्या उद्घाटन भाषणात, मुलींच्या सक्षमीकरणासाठी "लेक लाडकी" नावाची नवीन योजना सुरू करण्यात येणार असल्याची घोषणा करण्यात आली. या योजनेअंतर्गत, हापवाला आणि के शरी शिधापत्रिका असलेल्या कुटुंबातील मुलींना त्यांच्या जन्मानंतर अनुदान मिळेल आणि लाभार्थी मुलींना वयाची १८ वर्षे पूर्ण झाल्यावर ७५,००० रुपये रोख दिले जातील. परिणामी, "लेक लाडकी" सुरू करण्याचा निर्णय घेण्यात आला आहे. राज्यातील मुलींच्या सक्षमीकरणाची योजना सरकारकडे आहे.</p>
+</div>
+<div><p className='font-bold text-xl mt-4'>लेक लाडकी योजना महाराष्ट्र 2023 के लाभ व वैशिष्ट्ये:</p></div>
     </div>
     </div>
+ 
   )
 }
 
