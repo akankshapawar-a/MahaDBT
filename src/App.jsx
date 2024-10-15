6b7c12d1bd0feb2d8b657b15ad4f8b0588e6dc92
akanksha pawar <akankshapawar131202@gmail.com>
2024-10-15 18:04:16 +0530

    google manager

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import InformationData from './components/InformationData'
 import Navbar from './components/Navbar'
 import Comment from './components/Comment'
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
 
   return (
+    <HelmetProvider>
+
    <>
   <div>
     <Navbar/>
@@ -15,6 +18,8 @@ function App() {
     </div>
   </div>
    </>
+   </HelmetProvider>
+
   )
 }
 
